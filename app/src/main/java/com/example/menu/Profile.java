cbff2a7c8f15fd3df02a3b97d464c7c7bd99c559
RPLSKENSA <johanpbrc@gmail.com>
2021-11-23 08:32:11 +0700

    Update Tugas

--- app/src/main/java/com/example/menu/Profile.java
+++ app/src/main/java/com/example/menu/Profile.java
@@ -2,13 +2,30 @@ package com.example.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Profile extends AppCompatActivity {
+    //Mendeklarasi atau Membuat Variabel
+    ImageView img1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //Menghubungkan Variabel
+        img1 = findViewById(R.id.home);
+
+        //Aksi Variabel
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent send = new Intent(Profile.this, MainActivity.class);
+                startActivity(send);
+            }
+        });
     }
 }
